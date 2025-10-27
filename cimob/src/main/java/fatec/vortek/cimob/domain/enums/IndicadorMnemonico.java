374993187bdc75381d79fb349a6da94d7890920c
Thiago <100661398+thiagosabreu@users.noreply.github.com>
2025-10-26 22:48:36 -0300

    feat (vtk-100): Adicionado novos indicadores e atualizado Mnemonico, alterado metodo na repository

--- cimob/src/main/java/fatec/vortek/cimob/domain/enums/IndicadorMnemonico.java
+++ cimob/src/main/java/fatec/vortek/cimob/domain/enums/IndicadorMnemonico.java
@@ -3,7 +3,11 @@ package fatec.vortek.cimob.domain.enums;
 public enum IndicadorMnemonico {
     EXCESSO_VELOCIDADE("Excesso de Velocidade Grave"),
     VARIABILIDADE_VELOCIDADE("Variabilidade de Velocidade"),
-    VEICULOS_LENTOS("Veículos Muito Lentos");
+    VEICULOS_LENTOS("Veículos Muito Lentos"),
+    FLUXO_VEICULOS("Fluxo de Veículos"),
+    DIFERENCA_MEDIA_VELOCIDADE("Diferença Média de Velocidade"),
+    HOMOGENEIDADE_VELOCIDADE("Coeficiente de Homogeneidade de Velocidade"),
+    FREQUENCIA_PICOS_HORA("Frequência de Picos por Hora");
 
     private final String descricao;
 
--- cimob/src/main/java/fatec/vortek/cimob/infrastructure/repository/IndicadorRepository.java
+++ cimob/src/main/java/fatec/vortek/cimob/infrastructure/repository/IndicadorRepository.java
@@ -2,6 +2,7 @@ package fatec.vortek.cimob.infrastructure.repository;
 
 import fatec.vortek.cimob.domain.enums.IndicadorMnemonico;
 import fatec.vortek.cimob.domain.model.Indicador;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,10 +19,8 @@ public interface IndicadorRepository extends JpaRepository<Indicador, Long> {
     Indicador findByMnemonico(IndicadorMnemonico mnemonico);
     
     @Modifying
-    @Query("UPDATE Indicador i SET i.oculto = :valor")
-    void updateAllOculto(@Param("valor") String valor);
+    @Transactional
+    @Query("UPDATE Indicador i SET i.oculto = CASE WHEN i.indicadorId IN :ids THEN 'N' ELSE 'S' END")
+    void updateOcultoByIdsCase(@Param("ids") List<Long> ids);
 
-    @Modifying
-    @Query("UPDATE Indicador i SET i.oculto = :valor WHERE i.indicadorId IN :ids")
-    void updateOcultoByIds(@Param("valor") String valor, @Param("ids") List<Long> ids);
 }
